1f42787c00c1c9cc8e77231e745bc9e44ed4c1f3
Phil <prbush@uw.edu>
2022-10-28 16:48:50 +0000

    adding class files

---
+++ microSWIFT_V2/Core/Inc/GPS.h
@@ -0,0 +1,17 @@
+/*
+ * GPS.h
+ *
+ *  Created on: Oct 28, 2022
+ *      Author: Phil
+ */
+
+#ifndef SRC_GPS_H_
+#define SRC_GPS_H_
+
+class GPS {
+public:
+	GPS();
+	virtual ~GPS();
+};
+
+#endif /* SRC_GPS_H_ */
---
+++ microSWIFT_V2/Core/Src/GPS.cpp
@@ -0,0 +1,18 @@
+/*
+ * GPS.cpp
+ *
+ *  Created on: Oct 28, 2022
+ *      Author: Phil
+ */
+
+#include "GPS.h"
+
+GPS::GPS() {
+	// TODO Auto-generated constructor stub
+
+}
+
+GPS::~GPS() {
+	// TODO Auto-generated destructor stub
+}
+
