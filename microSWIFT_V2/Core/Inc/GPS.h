5bb0853f36200e9b1cebaba8f0fd76f016e36113
Phil <prbush@uw.edu>
2022-10-28 18:52:20 +0000

    starting on GPS class

--- microSWIFT_V2/Core/Inc/GPS.h
+++ microSWIFT_V2/Core/Inc/GPS.h
@@ -7,15 +7,20 @@
 
 #ifndef SRC_GPS_H_
 #define SRC_GPS_H_
+#include <stdint.h>
 
 class GPS {
 public:
 	GPS();
-	virtual ~GPS();
+	virtual ~GPS(UART_HandleTypeDef *uart_handle);
 	bool init(void);
 
-private:
+	bool sleep(void);
 
+private:
+	UART_HandleTypeDef uart_handle;
+	const char* search = "$GPGGA";
+	bool checksum(uint8_t CK_A, uint8_t CK_B, char* bytes, uint32_t bufferLength);
 };
 
 #endif /* SRC_GPS_H_ */
--- microSWIFT_V2/Core/Src/GPS.cpp
+++ microSWIFT_V2/Core/Src/GPS.cpp
@@ -3,6 +3,10 @@
  *
  *  Created on: Oct 28, 2022
  *      Author: Phil
+ *
+ *  We need to figure out if we will use UBX or NMEA messages
+ *  	- If we use UBX (preferred), UBX-NAV-PVT (Navigation position velocity time solution)
+ *  	  will be the ticket since it is periodic and can be polled.
  */
 
 #include "GPS.h"
@@ -18,5 +22,17 @@ GPS::~GPS() {
 
 bool GPS::init(void) {
 
+}
+
+bool GPS::checksum(uint8_t CK_A, uint8_t CK_B, char* bytes, uint32_t bufferLength) {
+	/* From u-blox Interface Description
+	 *  CK_A = 0, CK_B = 0
+		For (I = 0; I < N; I++) {
+			CK_A = CK_A + Buffer[I]
+			CK_B = CK_B + CK_A
+		}
+	 *
+	 * We need two uint8_t's in order to compare them with CK_A, CK_B
+	 */
 }
 
