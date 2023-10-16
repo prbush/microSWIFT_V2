c583822634a9b6fb696eb1f727628fe2813cf286
Phil <prbush@uw.edu>
2023-10-16 16:20:52 -0700

    testing new nedwaves.

---
+++ microSWIFT_U5/Core/Inc/NEDWaves/std.h
@@ -0,0 +1,36 @@
+/*
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ * File: std.h
+ *
+ * MATLAB Coder version            : 5.4
+ * C/C++ source code generated on  : 10-Oct-2023 20:23:55
+ */
+
+#ifndef STD_H
+#define STD_H
+
+/* Include Files */
+#include "NEDwaves_memlight_types.h"
+#include "rtwtypes.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Function Declarations */
+float b_std(const emxArray_real32_T *x);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+/*
+ * File trailer for std.h
+ *
+ * [EOF]
+ */
