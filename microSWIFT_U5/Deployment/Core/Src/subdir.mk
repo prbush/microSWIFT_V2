0102082a0f9d38307843ec3d5a1316ab180e3ca9
philbush5 <63520476+philbush5@users.noreply.github.com>
2024-01-17 11:50:30 -0800

    starting new test for ICE-PPR using LPTIM for wakeup

---
+++ microSWIFT_U5/Deployment/Core/Src/subdir.mk
@@ -0,0 +1,68 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (11.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Core/Src/app_threadx.c \
+../Core/Src/byte_array.c \
+../Core/Src/flash_storage.c \
+../Core/Src/linked_list.c \
+../Core/Src/main.c \
+../Core/Src/stm32u5xx_hal_msp.c \
+../Core/Src/stm32u5xx_hal_timebase_tim.c \
+../Core/Src/stm32u5xx_it.c \
+../Core/Src/syscalls.c \
+../Core/Src/sysmem.c \
+../Core/Src/system_stm32u5xx.c 
+
+S_UPPER_SRCS += \
+../Core/Src/tx_initialize_low_level.S 
+
+OBJS += \
+./Core/Src/app_threadx.o \
+./Core/Src/byte_array.o \
+./Core/Src/flash_storage.o \
+./Core/Src/linked_list.o \
+./Core/Src/main.o \
+./Core/Src/stm32u5xx_hal_msp.o \
+./Core/Src/stm32u5xx_hal_timebase_tim.o \
+./Core/Src/stm32u5xx_it.o \
+./Core/Src/syscalls.o \
+./Core/Src/sysmem.o \
+./Core/Src/system_stm32u5xx.o \
+./Core/Src/tx_initialize_low_level.o 
+
+S_UPPER_DEPS += \
+./Core/Src/tx_initialize_low_level.d 
+
+C_DEPS += \
+./Core/Src/app_threadx.d \
+./Core/Src/byte_array.d \
+./Core/Src/flash_storage.d \
+./Core/Src/linked_list.d \
+./Core/Src/main.d \
+./Core/Src/stm32u5xx_hal_msp.d \
+./Core/Src/stm32u5xx_hal_timebase_tim.d \
+./Core/Src/stm32u5xx_it.d \
+./Core/Src/syscalls.d \
+./Core/Src/sysmem.d \
+./Core/Src/system_stm32u5xx.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Core/Src/app_threadx.o: ../Core/Src/app_threadx.c Core/Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m33 -std=gnu11 -DUSE_HAL_DRIVER -DSTM32U575xx -DTX_INCLUDE_USER_DEFINE_FILE -DTX_SINGLE_MODE_NON_SECURE=1 -DNOT_DEBUG -DDEBUG -c -I../Core/Inc -I../Drivers/STM32U5xx_HAL_Driver/Inc -I../Drivers/STM32U5xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32U5xx/Include -I../Drivers/CMSIS/Include -I../AZURE_RTOS/App -I../Middlewares/ST/threadx/common/inc -I../Middlewares/ST/threadx/ports/cortex_m33/gnu/inc -I../Middlewares/ST/threadx/utility/low_power -I"C:/Users/veteran/STM32CubeIDE/microSWIFT_V2/microSWIFT_U5/Core/Inc/Waves" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv5-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Core/Src/%.o Core/Src/%.su Core/Src/%.cyclo: ../Core/Src/%.c Core/Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m33 -std=gnu11 -DUSE_HAL_DRIVER -DSTM32U575xx -DTX_INCLUDE_USER_DEFINE_FILE -DTX_SINGLE_MODE_NON_SECURE=1 -DNOT_DEBUG -DDEBUG -c -I../Core/Inc -I../Drivers/STM32U5xx_HAL_Driver/Inc -I../Drivers/STM32U5xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32U5xx/Include -I../Drivers/CMSIS/Include -I../AZURE_RTOS/App -I../Middlewares/ST/threadx/common/inc -I../Middlewares/ST/threadx/ports/cortex_m33/gnu/inc -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv5-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Core/Src/%.o: ../Core/Src/%.S Core/Src/subdir.mk
+	arm-none-eabi-gcc -mcpu=cortex-m33 -DTX_SINGLE_MODE_NON_SECURE=1 -DDEBUG -c -x assembler-with-cpp -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv5-sp-d16 -mfloat-abi=hard -mthumb -o "$@" "$<"
+
+clean: clean-Core-2f-Src
+
+clean-Core-2f-Src:
+	-$(RM) ./Core/Src/app_threadx.cyclo ./Core/Src/app_threadx.d ./Core/Src/app_threadx.o ./Core/Src/app_threadx.su ./Core/Src/byte_array.cyclo ./Core/Src/byte_array.d ./Core/Src/byte_array.o ./Core/Src/byte_array.su ./Core/Src/flash_storage.cyclo ./Core/Src/flash_storage.d ./Core/Src/flash_storage.o ./Core/Src/flash_storage.su ./Core/Src/linked_list.cyclo ./Core/Src/linked_list.d ./Core/Src/linked_list.o ./Core/Src/linked_list.su ./Core/Src/main.cyclo ./Core/Src/main.d ./Core/Src/main.o ./Core/Src/main.su ./Core/Src/stm32u5xx_hal_msp.cyclo ./Core/Src/stm32u5xx_hal_msp.d ./Core/Src/stm32u5xx_hal_msp.o ./Core/Src/stm32u5xx_hal_msp.su ./Core/Src/stm32u5xx_hal_timebase_tim.cyclo ./Core/Src/stm32u5xx_hal_timebase_tim.d ./Core/Src/stm32u5xx_hal_timebase_tim.o ./Core/Src/stm32u5xx_hal_timebase_tim.su ./Core/Src/stm32u5xx_it.cyclo ./Core/Src/stm32u5xx_it.d ./Core/Src/stm32u5xx_it.o ./Core/Src/stm32u5xx_it.su ./Core/Src/syscalls.cyclo ./Core/Src/syscalls.d ./Core/Src/syscalls.o ./Core/Src/syscalls.su ./Core/Src/sysmem.cyclo ./Core/Src/sysmem.d ./Core/Src/sysmem.o ./Core/Src/sysmem.su ./Core/Src/system_stm32u5xx.cyclo ./Core/Src/system_stm32u5xx.d ./Core/Src/system_stm32u5xx.o ./Core/Src/system_stm32u5xx.su ./Core/Src/tx_initialize_low_level.d ./Core/Src/tx_initialize_low_level.o
+
+.PHONY: clean-Core-2f-Src
+
