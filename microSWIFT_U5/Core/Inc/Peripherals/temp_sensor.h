6e37252b9034b690ce60ca798bf07b444902f9cc
Phil <prbush@uw.edu>
2024-03-25 08:14:38 -0700

    brought over temp_sensor.c/.h

---
+++ microSWIFT_U5/Core/Inc/Peripherals/temp_sensor.h
@@ -0,0 +1,56 @@
+/*
+ * temp_sensor.h
+ *
+ * !!! MOSTLY COPPIED FROM BLUE ROBOTICS !!!
+ *
+ * https://github.com/bluerobotics/BlueRobotics_TSYS01_Library?tab=readme-ov-file
+ *
+ *  Created on: Feb 9, 2024
+ *      Author: Phil
+ */
+
+#ifndef INC_PERIPHERALS_TEMP_SENSOR_H_
+#define INC_PERIPHERALS_TEMP_SENSOR_H_
+
+#include "stm32u5xx_hal.h"
+#include "tx_api.h"
+#include "main.h"
+#include "stm32u5xx_hal_i2c.h"
+#include "stm32u5xx_hal_i2c_ex.h"
+#include "stdbool.h"
+
+#define TSYS01_ADDR                        0x77 << 1
+#define TSYS01_RESET                       0x1E
+#define TSYS01_ADC_READ                    0x00
+#define TSYS01_ADC_TEMP_CONV               0x48
+#define TSYS01_PROM_READ                   0XA0
+
+typedef enum tmperature_error_code {
+	TEMPERATURE_SUCCESS = 0,
+	TEMPERATURE_CONVERSION_ERROR = -1,
+	TEMPERATURE_TIMEOUT_ERROR = -2,
+	TEMPERATURE_COMMUNICATION_ERROR = -3
+}temperature_error_code_t;
+
+typedef struct Temperature {
+	I2C_HandleTypeDef* 			i2c_handle;
+	TX_EVENT_FLAGS_GROUP*   control_flags;
+	TX_EVENT_FLAGS_GROUP*   error_flags;
+
+	void 										(*on)(void);
+	void										(*off)(void);
+	temperature_error_code_t(*reset_i2c)(void);
+	temperature_error_code_t(*self_test)(void);
+	temperature_error_code_t(*get_reading)(float* temp);
+
+	float										converted_temp;
+	float 								  C[8]; // Cal data array
+	uint32_t								D1; // Read data (unconverted temp)
+	uint32_t								adc;
+}Temperature;
+
+
+void temperature_init(Temperature* struct_ptr, I2C_HandleTypeDef* i2c_handle, TX_EVENT_FLAGS_GROUP* control_flags,
+		TX_EVENT_FLAGS_GROUP* error_flags, bool clear_calibration_data);
+
+#endif /* INC_PERIPHERALS_TEMP_SENSOR_H_ */
---
+++ microSWIFT_U5/Core/Src/Peripherals/temp_sensor.c
@@ -0,0 +1,196 @@
+/*
+ * temp_sensor.c
+ *
+ * !!! MOSTLY COPPIED FROM BLUE ROBOTICS !!!
+ *
+ * https://github.com/bluerobotics/BlueRobotics_TSYS01_Library?tab=readme-ov-file
+ *
+ *  Created on: Feb 9, 2024
+ *      Author: Phil
+ */
+
+#include "Peripherals/temp_sensor.h"
+// Object instance pointer
+static Temperature* self;
+
+// Struct functions
+static void						temperature_on(void);
+static void						temperature_off(void);
+static temperature_error_code_t	temperature_reset_i2c(void);
+static temperature_error_code_t temperature_self_test(void);
+static temperature_error_code_t temperature_get_reading(float* temp);
+
+// Helper functions
+static bool						init_sensor(void);
+static void 					calculate_temp(void);
+static void						reset_struct_fields(bool reset_calibration);
+
+void temperature_init(Temperature* struct_ptr, I2C_HandleTypeDef* i2c_handle, TX_EVENT_FLAGS_GROUP* control_flags,
+		TX_EVENT_FLAGS_GROUP* error_flags, bool clear_calibration_data)
+{
+	self = struct_ptr;
+
+	self->i2c_handle = i2c_handle;
+	self->control_flags = control_flags;
+	self->error_flags = error_flags;
+
+	self->on = temperature_on;
+	self->off = temperature_off;
+	self->reset_i2c = temperature_reset_i2c;
+	self->self_test = temperature_self_test;
+	self->get_reading = temperature_get_reading;
+
+	reset_struct_fields(clear_calibration_data);
+}
+
+
+static void	temperature_on(void)
+{
+	HAL_GPIO_WritePin(TEMP_PWR_GPIO_Port, TEMP_PWR_Pin, GPIO_PIN_SET);
+	tx_thread_sleep(1);
+}
+
+
+static void	temperature_off(void)
+{
+	HAL_GPIO_WritePin(TEMP_PWR_GPIO_Port, TEMP_PWR_Pin, GPIO_PIN_RESET);
+}
+
+
+static temperature_error_code_t	temperature_reset_i2c(void)
+{
+
+	temperature_error_code_t return_code = TEMPERATURE_SUCCESS;
+
+	HAL_I2C_DeInit(self->i2c_handle);
+
+	tx_thread_sleep(1);
+
+	self->i2c_handle->Instance = I2C2;
+	self->i2c_handle->Init.Timing = 0x40000A0B;
+	self->i2c_handle->Init.OwnAddress1 = 0;
+	self->i2c_handle->Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+	self->i2c_handle->Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+	self->i2c_handle->Init.OwnAddress2 = 0;
+	self->i2c_handle->Init.OwnAddress2Masks = I2C_OA2_NOMASK;
+	self->i2c_handle->Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+	self->i2c_handle->Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(self->i2c_handle) != HAL_OK)
+  {
+  	return_code = TEMPERATURE_COMMUNICATION_ERROR;
+  }
+
+  if (HAL_I2CEx_ConfigAnalogFilter(self->i2c_handle, I2C_ANALOGFILTER_ENABLE) != HAL_OK)
+  {
+  	return_code = TEMPERATURE_COMMUNICATION_ERROR;
+  }
+
+  if (HAL_I2CEx_ConfigDigitalFilter(self->i2c_handle, 0) != HAL_OK)
+  {
+  	return_code = TEMPERATURE_COMMUNICATION_ERROR;
+  }
+
+  return return_code;
+}
+
+
+static temperature_error_code_t temperature_self_test(void)
+{
+	if (init_sensor()) {
+		return TEMPERATURE_SUCCESS;
+	} else {
+		return TEMPERATURE_COMMUNICATION_ERROR;
+	}
+}
+
+
+static temperature_error_code_t temperature_get_reading(float* temp)
+{
+	temperature_error_code_t return_code = TEMPERATURE_SUCCESS;
+	uint8_t command = TSYS01_ADC_TEMP_CONV;
+	uint8_t read_data[3] = {0};
+	*temp = 0.0f;
+
+	if (HAL_I2C_Master_Transmit(self->i2c_handle, TSYS01_ADDR, &command, sizeof(command), 10)
+			!= HAL_OK) {
+		return_code = TEMPERATURE_CONVERSION_ERROR;
+		return return_code;
+	}
+
+	tx_thread_sleep(1);
+
+	command = TSYS01_ADC_READ;
+	if (HAL_I2C_Master_Transmit(self->i2c_handle, TSYS01_ADDR, &command, sizeof(command), 10)
+			!= HAL_OK) {
+		return_code = TEMPERATURE_CONVERSION_ERROR;
+		return return_code;
+	}
+
+	if (HAL_I2C_Master_Receive(self->i2c_handle, TSYS01_ADDR, &(read_data[0]), sizeof(read_data), 10)
+			!= HAL_OK) {
+		return_code = TEMPERATURE_CONVERSION_ERROR;
+		return return_code;
+	}
+
+	self->D1 = (read_data[0] << 16) | (read_data[1] << 8) | read_data[2];
+
+	calculate_temp();
+
+	*temp = self->converted_temp;
+
+	return return_code;
+}
+
+
+static bool	init_sensor(void)
+{
+	uint8_t command = TSYS01_RESET;
+	uint8_t read_data[2] = {0};
+	// Reset the TSYS01, per datasheet
+	if (HAL_I2C_Master_Transmit(self->i2c_handle, TSYS01_ADDR, &command, sizeof(command), 10)
+			!= HAL_OK) {
+		return false;
+	}
+
+	tx_thread_sleep(1);
+		// Read calibration values
+	for ( uint8_t i = 0 ; i < 8 ; i++ ) {
+		command = TSYS01_PROM_READ + (i*2);
+		if (HAL_I2C_Master_Transmit(self->i2c_handle, TSYS01_ADDR, &command, sizeof(command), 10)
+				!= HAL_OK) {
+			return false;
+		}
+
+		if (HAL_I2C_Master_Receive(self->i2c_handle, TSYS01_ADDR, &(read_data[0]), sizeof(read_data), 10)
+				!= HAL_OK) {
+			return false;
+		}
+
+		self->C[i] = (float)((read_data[0] << 8) | read_data[1]);
+	}
+
+	return true;
+}
+
+
+void calculate_temp(void)
+{
+	self->adc = self->D1/256;
+	self->converted_temp = (-2.0f) * self->C[1] / 1000000000000000000000.0f
+													* pow(self->adc,4) + 4.0f * self->C[2] / 10000000000000000.0f
+													* pow(self->adc,3) + (-2.0f) * self->C[3] / 100000000000.0f
+													* pow(self->adc,2) + 1.0f * self->C[4] / 1000000.0f
+													* self->adc + (-1.5f) * self->C[5] / 100.0f ;
+
+}
+
+static void	reset_struct_fields(bool reset_calibration)
+{
+	if (reset_calibration) {
+		memset(self->C, 0, sizeof(self->C));
+	}
+
+	self->converted_temp = 0.0f;
+	self->D1 = 0;
+	self->adc = 0;
+}
